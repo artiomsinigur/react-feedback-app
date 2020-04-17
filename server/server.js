93ff38b2511beed6cb98456b39a1664b56168171
Artiom <sinigur2006@gmail.com>
2020-04-17 15:06:29 -0400

    Publish on both

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send( {say: 'Hello publish only on heroku'} ))
+app.get('/', (req, res) => res.send( {say: 'Hello now publish on both'} ))
 
 app.listen(PORT, () => console.info('Server is up on port ' + PORT))
